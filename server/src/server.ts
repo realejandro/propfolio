b7225a68b86f0e652362a0abf3fc8cb62b3f57e1
Narupo <pouzumaki@gmail.com>
2025-03-26 21:40:06 -0500

    small readme update, fixed dirname issue within server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
-
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './services/auth.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
