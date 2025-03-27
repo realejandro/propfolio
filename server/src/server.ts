ba9dbe5926a60693b84706a45db292bd1de1029f
Narupo <pouzumaki@gmail.com>
2025-03-26 21:47:02 -0500

    fixed pathing with dirname

--- server/src/server.ts
+++ server/src/server.ts
@@ -33,10 +33,10 @@ const startApolloServer = async () => {
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
     
     app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
   
